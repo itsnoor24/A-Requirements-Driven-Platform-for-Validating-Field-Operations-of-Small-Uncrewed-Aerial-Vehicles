ee2fd9f631ec1a7c7001e4ab0f4762f023469dbb
Ramin <ra.sharifi1995@gmail.com>
2024-12-16 10:20:56 -0800

    re-writing the collision monitor

--- backend/PythonClient/multirotor/monitor/collision_monitor.py
+++ backend/PythonClient/multirotor/monitor/collision_monitor.py
@@ -1,9 +1,9 @@
 import threading
-
 from time import sleep
-
 from PythonClient.multirotor.mission.fly_straight import FlyStraight
-from PythonClient.multirotor.monitor.abstract.single_drone_mission_monitor import SingleDroneMissionMonitor
+from PythonClient.multirotor.monitor.abstract.single_drone_mission_monitor import (
+    SingleDroneMissionMonitor,
+)
 
 
 class CollisionMonitor(SingleDroneMissionMonitor):
@@ -12,61 +12,52 @@ class CollisionMonitor(SingleDroneMissionMonitor):
         super().__init__(mission)
 
     def start(self):
-        self.append_info_to_log(f"{self.target_drone};CollisionMonitor started")
+        self.log_info(f"{self.target_drone}; CollisionMonitor started")
         prev_collision = None
         time_interval = 3.0
-        collided = False
+        collision_detected = False
+
         while self.mission.state != self.mission.State.END:
             collision_info = self.client.simGetCollisionInfo(self.target_drone)
+
             if collision_info.has_collided:
-                if prev_collision is None or prev_collision != collision_info.object_name:
-                    collision_abs_location = self.client.simGetObjectPose(object_name=self.target_drone).position
-                    self.append_fail_to_log(
-                        f"{self.target_drone};collided with {collision_info.object_name} at absolute position"
-                        f"x = {collision_abs_location.x_val} meters, "
-                        f"y = {collision_abs_location.y_val} meters, "
-                        f"z = {collision_abs_location.z_val} meters]")
-                    collided = True
-                    self.save_image(collision_info.object_name)
+                if (
+                    prev_collision is None
+                    or prev_collision != collision_info.object_name
+                ):
+                    collision_location = self.client.simGetObjectPose(
+                        self.target_drone
+                    ).position
+                    self.log_fail(
+                        f"{self.target_drone}; Collided with {collision_info.object_name} "
+                        f"at position x = {collision_location.x_val} m, "
+                        f"y = {collision_location.y_val} m, "
+                        f"z = {collision_location.z_val} m"
+                    )
+
+                    collision_detected = True
+                    self.capture_image(collision_info.object_name)
                     prev_collision = collision_info.object_name
+
             sleep(time_interval)
-        if not collided:
-            self.append_pass_to_log(f"{self.target_drone};No collision detected")
+
+        if not collision_detected:
+            self.log_pass(f"{self.target_drone}; No collision detected")
         else:
-            self.append_fail_to_log(f"{self.target_drone};Collision detected")
+            self.log_fail(f"{self.target_drone}; Collision detected")
+
         self.save_report()
-
-    def save_image(self, collision_object):
-        # # will revisit, currently causing issue
-        pass
-
-        # requests = [airsim.ImageRequest("0", airsim.ImageType.DepthVis),  # depth visualization image
-        #             airsim.ImageRequest("1", airsim.ImageType.DepthPerspective, True),
-        #             # depth in perspective projection
-        #             airsim.ImageRequest("1", airsim.ImageType.Scene),  # scene vision image in png format
-        #             airsim.ImageRequest("1", airsim.ImageType.Scene, False,
-        #                                 False)]  # scene vision image in uncompressed RGBA array
-        # client = airsim.MultirotorClient()
-        # responses = client.simGetImages(requests, vehicle_name=self.target_drone)
-        #
-        # graph_dir = self.get_graph_dir()
-        # print("Saving images to %s" % graph_dir)
-        # if not os.path.exists(graph_dir):
-        #     os.makedirs(graph_dir)
-        # for idx, response in enumerate(responses):
-        #     filename = os.path.join(graph_dir, str(idx))
-        #     if response.pixels_as_float:
-        #         airsim.write_pfm(os.path.normpath(filename + collision_object + '.pfm'), airsim.get_pfm_array(response))
-        #     else:
-        #         airsim.write_file(os.path.normpath(filename + collision_object + '.png'), response.image_data_uint8)
 
 
 if __name__ == "__main__":
     drone_speed = 14
-    point = [10, 10, -0.5]
-    mission = FlyStraight(destination=point, speed=drone_speed)
+    destination = [10, 10, -0.5]
+    mission = FlyStraight(destination=destination, speed=drone_speed)
     monitor = CollisionMonitor(mission)
-    thread1 = threading.Thread(target=mission.start)
-    thread2 = threading.Thread(target=monitor.start)
-    thread1.start()
-    thread2.start()
+
+    # Launching the mission and monitor in parallel threads
+    mission_thread = threading.Thread(target=mission.start)
+    monitor_thread = threading.Thread(target=monitor.start)
+
+    mission_thread.start()
+    monitor_thread.start()
